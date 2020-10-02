f30f05cef82d33f4ba6f6a61ff8f804b441e0e58
takker99 <37929109+takker99@users.noreply.github.com>
2020-10-03 00:07:35 +0900

    :sparkles: csvからデータを読み込むようにした

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import numpy as np
+import pandas as pd
 import math
 from typing import Sequence
+import argparse
 
 
 def makeA(c: list):
@@ -76,19 +78,32 @@ def printTheta(angles: Sequence[float], step: int, indent: int = 1, symbol: str 
     for i in range(len(angles)):
         print(f'{space}{symbol}_{i}({step}) = {todegree(angles[i],4)}')
 
-    # main函数
+
+# main函数
 if __name__ == '__main__':
+    # commald-line argumentsの設定
+    parser = argparse.ArgumentParser(
+        description='三角測量によって得た四辺形鎖の8個の角(radian)の最確値と残差を求めるscript')
+    parser.add_argument('input_file', help='角の値が書き込まれたcsvファイル')
+    parser.add_argument('--ignore', '-I', choices=[
+                        'header', 'index', 'both'], default='none', help='csv fileのheader及びindexを無視するかどうか')
+    args = parser.parse_args()
+
+    df=None
+
+    # fileを読み込む
+    if args.ignore == 'header':
+        df = pd.read_csv(args.input_file, usecols=[0])
+    elif args.ignore == 'index':
+        df = pd.read_csv(args.input_file, index_col=0, header=None, usecols=[0, 1])
+    elif args.ignore == 'both':
+        df = pd.read_csv(args.input_file, index_col=0, usecols=[0, 1])
+    else:
+        df = pd.read_csv(args.input_file, header=None, usecols=[0])
+
     # 初期値
-    theta = np.matrix([
-        [0.757206248],
-        [1.090661098],
-        [0.682819669],
-        [0.610873318],
-        [0.720344248],
-        [1.127482697],
-        [0.821040049],
-        [0.47273374],
-    ], dtype=float)
+    measured_angles = np.matrix(df, dtype=float)
+    theta = np.matrix(measured_angles);
 
     i = 0
     print('This program calculates angles in Radian')
@@ -106,7 +121,7 @@ if __name__ == '__main__':
         i += 1
         print(f'### result of step {i} ###')
         printTheta(*theta.T.tolist(), i)
-        printTheta(residuals[0], i, symbol='⊿')
+        printTheta(residual, i-1, symbol='⊿')
         if len(residuals) > 10:
             residuals.pop(0)
 
@@ -115,4 +130,4 @@ if __name__ == '__main__':
     print('Most probable angles:')
     printAngles(*theta.T.tolist())
     print('Residuals:')
-    printAngles(residuals[len(residuals)-1], symbol='ν')
+    printAngles(*(theta - measured_angles).T.tolist(), symbol='ν')
